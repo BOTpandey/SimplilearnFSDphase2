eb85a5746635ecb978f240a4076fad10b5b9bfc0
BOTpandey <69634796+BOTpandey@users.noreply.github.com>
2022-12-15 18:28:20 +0530

    Create quiz.js

---
+++ quiz.js
@@ -0,0 +1,54 @@
+let quiz=[
+    {
+        question:"Who developed Python Programming Language?",
+        option:[
+            "1.Wick van Rossum",
+            "2.Rasmus Lerdorf",
+            "3.Guido van Rossum",
+            "4.Niene Stom",
+        ],
+        answer:3,
+    },
+    {
+        question:"Which type of Programming does Python support?",
+        option:[
+            "1.object-oriented programming",
+            "2.structured programming",
+            "3.functional programming",
+            "4.all of the mentioned",
+            
+        ],
+        answer:4,
+    },
+    {
+        question:"Is Python case sensitive when dealing with identifiers?",
+        option:[
+            "1.No",
+            "2.Yes",
+            "3.Machine Dependent",
+            "4.None of the mentioned",   
+        ],
+        answer:2,
+    },
+    {
+        question:"Which of the following is the correct extension of the Python file?",
+        option:[
+            "1. .python",
+            "2. .pl ",
+            "3. .py ",
+            "4. .p ",   
+        ],
+        answer:3,
+
+    },
+    {
+        question:"Is Python code compiled or interpreted?",
+        option:[
+            "1.Python code is both compiled and interpreted",
+            "2.Python code is neither compiled nor interpreted",
+            "3.Python code is only compiled",
+            "4.Python code is only interpreted",   
+        ],
+        answer:1,
+    }
+]
